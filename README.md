7a5c78995691379c95a1a244afe1bfcbc81b70fb
BAgBAn <bagbanafzal@gmail.com>
2018-02-26 00:21:49 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PyJobFeed
 Python app to fetch latest data from job portal website [http://indeed.co.in/]
+
+Demo video [output] : https://youtu.be/Cnioq4bQzBA
--- code/Jobs.py
+++ code/Jobs.py
@@ -1,3 +1,4 @@
 # Demo file Monday 26 February 2018 12:01:26 AM IST 
 # This app is based on Rss feed [xml] and feedparser 5.2.1 modules
 # https://pypi.python.org/pypi/feedparser
+# find video here : https://youtu.be/Cnioq4bQzBA
