3ab01d091a151a93b3841601941577ae174751e2
BAgBAn <bagbanafzal@gmail.com>
2018-02-26 15:05:29 +0530

    Initial commit

--- code/Jobs.py
+++ code/Jobs.py
@@ -1,4 +1,4 @@
 # Demo file Monday 26 February 2018 12:01:26 AM IST 
-# This app is based on Rss feed [xml] and feedparser 5.2.1 modules
+# This app is based on Rss feed [xml] and feedparser 5.2.1 module
 # https://pypi.python.org/pypi/feedparser
-# find video here : https://youtu.be/Cnioq4bQzBA
+# find video here [Output] : https://youtu.be/Cnioq4bQzBA
---
+++ code/News.py
@@ -0,0 +1,4 @@
+# Demo file Monday 26 February 2018 
+# This app is based on Rss feed [xml] and feedparser 5.2.1 module
+# https://pypi.python.org/pypi/feedparser
+# find video here [Output]: 
